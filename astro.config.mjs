52440e0965e37f4376267d857d3a5ecb60822ea0
Platon Rov <platon.rov@gmail.com>
2025-03-09 19:26:41 +0100

    fix: theme toggler

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://platon-rov.github.io",
-  base: "/madrov-me",
+  base: "/platon-rov.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
