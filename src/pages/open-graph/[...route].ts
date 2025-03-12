236e673955c61af82ac81bf4148e2eb1696b128a
Platon Rov <platon.rov@gmail.com>
2025-03-12 22:20:13 +0100

    fix: open graph

--- src/pages/open-graph/[...route].ts
+++ src/pages/open-graph/[...route].ts
@@ -5,19 +5,12 @@ import { home } from "../constants";
 const collectionEntries = await getCollection("posts");
 
 export const { getStaticPaths, GET } = OGImageRoute({
-  // Tell us the name of your dynamic route segment.
-  // In this case it’s `route`, because the file is named `[...route].ts`.
   param: "route",
 
-  // Map the array of content collection entries to create an object.
-  // Converts [{ id: 'post.md', data: { title: 'Example', description: '' } }]
-  // to { 'post.md': { title: 'Example', description: '' } }
   pages: {
-    ...Object.fromEntries(collectionEntries.map(({ id, data }) => [id, data])),
-    ...{ home },
+    ...Object.fromEntries(collectionEntries.map(({ id, data }) => [id + ".html", data])),
+    ...{ "home.html": home },
   },
-
-  getSlug: (path) => path,
 
   getImageOptions: (_, page) => ({ title: page.title, description: page.description }),
 });
